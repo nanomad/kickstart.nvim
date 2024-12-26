074086060bdbd6d7736dfb1dcd2e0faa41a8cab9
Giovanni Condello <condellog@gmail.com>
2024-12-26 21:34:30 +0100

    Add Java and Elixir

--- init.lua
+++ init.lua
@@ -438,6 +438,84 @@ require('lazy').setup({
 
   -- LSP Plugins
   {
+    'mfussenegger/nvim-jdtls',
+    opts = {
+      root_markers = { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
+    },
+    config = function(_, opts)
+      local get_platform = function()
+        if vim.fn.has 'mac' then
+          return 'mac_arm'
+        elseif vim.fn.has 'win32' then
+          return 'win'
+        else
+          return 'linux'
+        end
+      end
+      local resolve_opts = function()
+        local root_dir = require('jdtls.setup').find_root(opts.root_markers)
+        local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+        local workspace_dir = vim.fn.stdpath 'cache' .. '/jdtls/workspace-root/' .. project_name
+        if vim.loop.fs_stat(workspace_dir) == nil then
+          os.execute('mkdir ' .. workspace_dir)
+        end
+        local install_path = require('mason-registry').get_package('jdtls'):get_install_path()
+        local platform = get_platform()
+
+        return {
+          cmd = {
+            'java',
+            '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+            '-Dosgi.bundles.defaultStartLevel=4',
+            '-Declipse.product=org.eclipse.jdt.ls.core.product',
+            '-Dlog.protocol=true',
+            '-Dlog.level=ALL',
+            '-javaagent:' .. install_path .. '/lombok.jar',
+            '-Xmx1g',
+            '--add-modules=ALL-SYSTEM',
+            '--add-opens',
+            'java.base/java.util=ALL-UNNAMED',
+            '--add-opens',
+            'java.base/java.lang=ALL-UNNAMED',
+            '-jar',
+            vim.fn.glob(install_path .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
+            '-configuration',
+            install_path .. '/config_' .. platform,
+            '-data',
+            workspace_dir,
+          },
+          root_dir = root_dir,
+        }
+      end
+      vim.api.nvim_create_autocmd('Filetype', {
+        pattern = 'java',
+        callback = function(event)
+          local start_opts = resolve_opts()
+          require('jdtls').start_or_attach(start_opts)
+          local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          end
+          map('<leader>cv', function()
+            vim.cmd 'normal! \28\14'
+            require('jdtls').extract_variable_all { visual = true, name = vim.fn.input 'Name > ' }
+          end, 'Extract [V]ariable', { 'v' })
+          map('<leader>cm', function()
+            vim.cmd 'normal! \28\14'
+            require('jdtls').extract_method { visual = true, name = vim.fn.input 'Name > ' }
+          end, 'Extract [M]ethod', { 'v' })
+          map('<leader>cc', function()
+            vim.cmd 'normal! \28\14'
+            require('jdtls').extract_constant { visual = true, name = vim.fn.input 'Name > ' }
+          end, 'Extract [C]onstant', { 'v' })
+          map('<leader>ci', function()
+            require('jdtls').organize_imports()
+          end, 'Organize [I]mports')
+        end,
+      })
+    end,
+  },
+  {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
     -- used for completion, annotations and signatures of Neovim apis
     'folke/lazydev.nvim',
@@ -614,9 +692,11 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        -- Java Language Server by Eclipse
+        jdtls = {},
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -662,6 +742,10 @@ require('lazy').setup({
       require('mason-lspconfig').setup {
         handlers = {
           function(server_name)
+            -- Avoid jdtls double registration
+            if server_name == 'jdtls' then
+              return true
+            end
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
@@ -784,7 +868,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<Tab>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
